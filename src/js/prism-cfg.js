d59bf80f50cf47dac93f8ab83bc17b10bc1c3f2e
XiaoK <xiaok@zxkxz.cn>
2024-06-07 09:30:01 +0800

    prism: 版权支持

--- src/js/prism-cfg.js
+++ src/js/prism-cfg.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) XiaoK <xiaok@zxkxz.cn>
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 Prism.languages.cfg = {
     'comment': [
         {
