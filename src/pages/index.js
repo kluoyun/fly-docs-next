cb6049688c00c2e35514e0486b9a687a4eda9299
Xiaok <xiaok@zxkxz.cn>
2025-03-24 13:26:53 +0800

    3dmellow.com上线

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,102 @@
+import React from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Translate, {translate} from '@docusaurus/Translate';
+import useBaseUrl, {useBaseUrlUtils} from '@docusaurus/useBaseUrl';
 
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+import Slider from "react-slick";
+
+
+function HeroBanner() {
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+    <div className={styles.hero} data-theme="dark">
+      <div className={styles.heroInner}>
+        <Heading as="h1" className={styles.heroProjectTagline}>
+          <img
+            alt={translate({message: 'mellow logo'})}
+            className={styles.heroLogo}
+            src={useBaseUrl('/img/Mz.png')}
+            width="300"
+            height="300"
+          />
+          <span
+            className={styles.heroTitleTextHtml}
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: translate({
+                id: 'homepage.hero.title',
+                message:
+                  '3D Mellow <b>专注于</b> 提供3D打印机 <b>全套</b> 解决方案',
+                description:
+                  'index title'
+              }),
+            }}
+          />
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            产品文档
+        <div className={styles.indexCtas}>
+          <Link className="button button--primary" to="/docs/intro">
+            <Translate>查看文档</Translate>
           </Link>
+          <Link className="button button--info" to="https://3dmellow.com/">
+            <Translate>3D Mellow Shop</Translate>
+          </Link>
+          <span className={styles.indexCtasGitHubButtonWrapper}>
+            <iframe
+              className={styles.indexCtasGitHubButton}
+              src="https://ghbtns.com/github-btn.html?user=kluoyun&repo=fly-docs-next&type=star&count=true&size=large"
+              width={160}
+              height={30}
+              title="GitHub Stars"
+            />
+          </span>
         </div>
       </div>
-    </header>
+    </div>
+  );
+}
+
+function TopBanner() {
+  // TODO We should be able to strongly type customFields
+  //  Refactor to use a CustomFields interface + TS declaration merging
+  const announcedVersion = useDocusaurusContext().siteConfig.customFields
+    ?.announcedVersion;
+
+  return (
+    <div className={styles.topBanner}>
+      <div className={styles.topBannerTitle}>
+        {'🎉\xa0'}
+        <Link
+          to="https://3dmellow.com/"
+          className={styles.topBannerTitleText}>
+          <Translate>
+            {'Mellow Shop\xa0现已上线!️'}
+          </Translate>
+        </Link>
+        {'\xa0🥳'}
+      </div>
+    </div>
   );
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const {
+    siteConfig: {customFields, tagline},
+  } = useDocusaurusContext();
+  const {description} = customFields;
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Klipper documentation for FLY products">
-      <HomepageHeader />
+    <Layout title={tagline} description={description}>
       <main>
-        <HomepageFeatures />
+        <TopBanner />
+        <HeroBanner />
+        <div className={styles.section}>
+          <HomepageFeatures />
+        </div>
       </main>
     </Layout>
   );
